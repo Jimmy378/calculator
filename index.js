f969cb91b0e2208b757470ee4e2f5f7fd0afa3b3
James Anderson <jamesanderson.contact@gmail.com>
2020-06-18 09:06:01 +1000

    updated logic

--- index.js
+++ index.js
@@ -1,114 +1,125 @@
-// class Calculator {
-//   displayValue = "0";
-//   firstOperand = null;
-//   waitingForSecondOperand = false;
-//   operator = null;
+class Calculator {
+  displayValue = "0";
+  firstOperand = null;
+  waitingForSecondOperand = false;
+  operator = null;
 
-//   constructor() {
-//     this.updateDisplay();
-//   }
+  constructor() {
+    this.updateDisplay();
+  }
 
-//   updateDisplay() {
-//     const display = document.querySelector(".result p");
-//     display.textContent = this.displayValue;
-//   }
+  updateDisplay() {
+    const display = document.querySelector(".result p");
+    display.textContent = this.displayValue;
+  }
 
-//   inputDigit(digit) {
-//     if (this.waitingForSecondOperand === true) {
-//       this.displayValue = digit;
-//       this.waitingForSecondOperand = false;
-//     } else {
-//       this.displayValue =
-//         this.displayValue === "0" ? digit : this.displayValue + digit;
-//     }
-//   }
+  inputDigit(digit) {
+    if (this.waitingForSecondOperand === true) {
+      this.displayValue = digit;
+      this.waitingForSecondOperand = false;
+    } else {
+      this.displayValue =
+        this.displayValue === "0" ? digit : this.displayValue + digit;
+    }
+  }
 
-//   inputDecimal(dot) {
-//     if (this.waitingForSecondOperand === true) return;
-//     if (!this.displayValue.includes(dot)) {
-//       this.displayValue += dot;
-//     }
-//   }
+  inputDecimal(dot) {
+    if (this.waitingForSecondOperand === true) return;
+    if (!this.displayValue.includes(dot)) {
+      this.displayValue += dot;
+    }
+  }
 
-//   performCalculation = {
-//     "÷": (firstOperand, secondOperand) => firstOperand / secondOperand,
+  performCalculation = (operator, firstOperand, secondOperand) => {
+    switch (operator) {
+      case "÷":
+        return firstOperand / secondOperand;
 
-//     "×": (firstOperand, secondOperand) => firstOperand * secondOperand,
+      case "×":
+        return firstOperand * secondOperand;
 
-//     "+": (firstOperand, secondOperand) => firstOperand + secondOperand,
+      case "+":
+        return firstOperand + secondOperand;
 
-//     "−": (firstOperand, secondOperand) => firstOperand - secondOperand,
+      case "−":
+        return firstOperand - secondOperand;
 
-//     "=": (firstOperand, secondOperand) => secondOperand,
-//   };
+      case "=":
+        return secondOperand;
 
-//   handleOperator(nextOperator) {
-//     const inputValue = parseFloat(this.displayValue);
+      default:
+        return secondOperand;
+    }
+  };
 
-//     if (this.operator && this.waitingForSecondOperand) {
-//       this.operator = nextOperator;
-//       return;
-//     }
+  handleOperator(nextOperator) {
+    const inputValue = parseFloat(this.displayValue);
 
-//     if (this.firstOperand == null) {
-//       this.firstOperand = inputValue;
-//     } else if (this.operator) {
-//       const result = this.performCalculation[this.operator](
-//         this.firstOperand,
-//         inputValue
-//       );
+    if (this.operator && this.waitingForSecondOperand) {
+      this.operator = nextOperator;
+      return;
+    }
 
-//       this.displayValue = String(result);
-//       this.firstOperand = result;
-//     }
+    if (this.firstOperand == null) {
+      this.firstOperand = inputValue;
+    } else if (this.operator) {
+      const result = this.performCalculation(
+        this.operator,
+        this.firstOperand,
+        inputValue
+      );
 
-//     this.waitingForSecondOperand = true;
-//     this.operator = nextOperator;
-//   }
+      this.displayValue = String(result);
+      this.firstOperand = result;
+    }
 
-//   reset() {
-//     this.displayValue = "0";
-//     this.firstOperand = null;
-//     this.waitingForSecondOperand = false;
-//     this.operator = null;
-//   }
+    this.waitingForSecondOperand = true;
+    this.operator = nextOperator;
+  }
 
-//   keyDown(type, value) {
-//     switch (type) {
-//       case "number":
-//         this.inputDigit(value);
-//         this.updateDisplay();
-//         break;
+  reset() {
+    this.displayValue = "0";
+    this.firstOperand = null;
+    this.waitingForSecondOperand = false;
+    this.operator = null;
+  }
 
-//       case "decimal":
-//         this.inputDecimal(value);
-//         this.updateDisplay();
-//         break;
+  keyDown(type, value) {
+    switch (type) {
+      case "number":
+        this.inputDigit(value);
+        this.updateDisplay();
+        break;
 
-//       case "clear":
-//         this.reset();
-//         this.updateDisplay();
-//         break;
+      case "decimal":
+        this.inputDecimal(value);
+        this.updateDisplay();
+        break;
 
-//       case "operator":
-//       case "equals":
-//         this.handleOperator(value);
-//         this.updateDisplay();
-//         break;
+      case "clear":
+        this.reset();
+        this.updateDisplay();
+        break;
 
-//       default:
-//         break;
-//     }
-//   }
-// }
+      case "operator":
+      case "equals":
+        this.handleOperator(value);
+        this.updateDisplay();
+        break;
 
-// let calculator = new Calculator();
+      default:
+        break;
+    }
+  }
+}
 
-// document.querySelectorAll(".button").forEach((button) => {
-//   button.addEventListener("click", () => {
-//     calculator.keyDown(button.getAttribute("type"), button.textContent);
-//   });
-//   button.addEventListener("touchstart", () => {
-//     calculator.keyDown(button.getAttribute("type"), button.textContent);
-//   });
-// });
+let calculator = new Calculator();
+
+document.querySelectorAll(".button").forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.keyDown(button.getAttribute("type"), button.textContent);
+  });
+  button.addEventListener("touchstart", () => {
+    calculator.keyDown(button.getAttribute("type"), button.textContent);
+  });
+});
